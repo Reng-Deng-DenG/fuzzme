1ff9262948493fe18054542adfc3522eec12ebe8
Reng-Deng-DenG <60116535+Reng-Deng-DenG@users.noreply.github.com>
2020-06-07 21:29:59 +0200

    Create index.php

---
+++ admin/index.php
@@ -0,0 +1,27 @@
+<?php session_start();
+include('../config.php');
+
+if(!isset($_SESSION['email']) && !isset($_SESSION['username']))
+{
+	header('Location: '.$uri.'/login.php');
+	exit();
+}
+elseif(!isset($_SESSION['rank']) && $_SESSION['rank'] === 0);
+{
+	header('Location: '.$uri.'/admin/login.php');
+	exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home | Admin Panel</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+
+</body>
+</html>
