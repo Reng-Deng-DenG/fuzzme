bd2547272099b61277c34d94c7275b5c746f792c
Reng-Deng-DenG <60116535+Reng-Deng-DenG@users.noreply.github.com>
2020-06-07 15:01:05 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/body.css">
+</head>
+<body>
+<h1>Home</h1>
+
+<div class='directory'>
+
+	<h3>Challenges <button>></button></h3>
+	<div class='directoryChild'>
+		<div class='d1'>
+		</div>
+	</div>
+
+	<h3>Labs</h3>
+	<div class='d2'>
+	</div>
+
+	<h3>Vulnerability</h3>
+	<div class='d3'>
+	</div>
+
+</div>
+
+
+<script type="text/javascript">
+
+
+function display_link(value){
+
+	var actual = document.getElementById(value).style.visibility
+
+
+}
+
+</script>
+</body>
+</html>
