135e319779abaf9a652351de64035dc6b23fe56f
Subnetix <57098204+Subnetix@users.noreply.github.com>
2020-06-07 22:44:53 +0200

    Update challenges.php

--- admin/challenges.php
+++ admin/challenges.php
@@ -1,1 +1,29 @@
+<html>
+	<head>
+		<link href="css/body.css" rel="stylesheet">
 
+	</head>
+	<body>
+	<table>
+   		<tr>
+       			<th>ID</th>
+       			<th>Name</th>
+       			<th>Type</th>
+       			<th>Difficulty</th>
+       			<th>Points</th>
+       			<th>Column</th>
+   		</tr>
+   		<tr>
+       			<td>Carmen</td>
+       			<td>33 ans</td>
+       			<td>Espagne</td>
+       			<td>eee</td>
+       			<td>aaa</td>
+       			<td>ggg</td>
+  		</tr>
+	</table>
+	<tr>
+        	<td><input type="submit" value="Go !"></td>
+        </tr>
+	</body>
+</html>
