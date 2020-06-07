c58b72ef9d158375e00f245f86ea1ce10d249ed5
Reng-Deng-DenG <60116535+Reng-Deng-DenG@users.noreply.github.com>
2020-06-07 21:01:43 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <div class='header'>
-	<h2 class='nav'> FUZZ<span class='greenMedium'>|</span>ME</h3>
+	<h2 class='nav'> FUZZ<span class='greenMedium'>|</span>ME</h2>
 	<a href='setting.php' class='nav'>Setting</a>
 	<a href='profil.php' class='nav'>Profil</a>
 	<a href='ranking.php' class='nav'>Ranking</a>
