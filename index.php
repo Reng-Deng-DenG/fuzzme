d6da0538c0afde877a4a11c49d8cb766cd33e1cc
Reng-Deng-DenG <60116535+Reng-Deng-DenG@users.noreply.github.com>
2020-06-07 19:53:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,31 @@
+<?php session_start();
+include('config.php');
+
+if(!isset($_SESSION['email']) && !isset($_SESSION['username']))
+{
+	header('Location: '.$uri.'/login.php');
+	exit();
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Home</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="css/body.css">
 </head>
 <body>
-<h1>Home</h1>
 
-<div class='directory'>
+<?php
+echo '<h1> TEST </h1>';
+echo htmlspecialchars($_SESSION['email']);
+echo '<br>';
+echo htmlspecialchars($_SESSION['username']);
 
-	<h3>Challenges <button>></button></h3>
-	<div class='directoryChild'>
-		<div class='d1'>
-		</div>
-	</div>
+var_dump($_SESSION);
 
-	<h3>Labs</h3>
-	<div class='d2'>
-	</div>
+?>
 
-	<h3>Vulnerability</h3>
-	<div class='d3'>
-	</div>
-
-</div>
-
-
-<script type="text/javascript">
-
-
-function display_link(value){
-
-	var actual = document.getElementById(value).style.visibility
-
-
-}
-
-</script>
 </body>
 </html>
