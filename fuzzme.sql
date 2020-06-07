74b253503003c392786b77f42bb8f31f090e0b8f
Reng-Deng-DenG <60116535+Reng-Deng-DenG@users.noreply.github.com>
2020-06-07 15:07:51 +0200

    Create fuzzme.sql

---
+++ fuzzme.sql
@@ -0,0 +1,42 @@
+CREATE USER 'fuzzme'@'localhost' IDENTIFIED WITH mysql_native_password BY 'secpassword!';
+CREATE DATABASE fuzzme;
+use fuzzme;
+GRANT ALL ON fuzzme.* TO 'fuzzme'@'localhost';
+
+
+CREATE TABLE `challenges`
+(
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `name` VARCHAR(60) NOT NULL,
+        `type` VARCHAR(60) NOT NULL,
+        `difficulty` VARCHAR(60) NOT NULL,
+        `points` INT NOT NULL,
+
+        PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `labs`
+(
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `name` INT NOT NULL,
+        `flawType` VARCHAR(120) NOT NULL,
+
+        PRIMARY KEY ('id')
+)
+
+
+CREATE TABLE `users`
+(
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`username` VARCHAR(60) NOT NULL,
+	`password` VARCHAR(60) NOT NULL,
+	`email` VARCHAR(60) NOT NULL,
+	`rank` INT NOT NULL,
+        `points` INT NOT NULL,
+
+	PRIMARY KEY(`id`)
+);
+
+INSERT INTO `users` (username, password, email, rank) VALUES ('admin', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'admin@gmail.com', '1');
+
+
