57758335b7b03c6a6675d0c570dda8987f72c808
Reng-Deng-DenG <60116535+Reng-Deng-DenG@users.noreply.github.com>
2020-06-07 19:17:59 +0200

    Update login.php

--- login.php
+++ login.php
@@ -30,8 +30,49 @@ if(isset($_SESSION['email']) && isset($_SESSION['username']))
 	</form>
 
 <?php
-
 include('db/db.php');
+
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$submit = $_POST['submit'];
+
+if(isset($email) && !empty($email) && isset($password) && !empty($password) && isset($submit))
+{
+	$r = $db->prepare('SELECT * FROM users WHERE email = ?');
+	$r->execute([$email]);
+	$data = $r->fetch();
+
+	if($data === true)// Vérification de l'email
+	{
+		if(sha1($password) === $data['password'])
+		{
+			try
+			{
+				$r = $db->prepare("UPDATE `users` SET ip = '$ip' WHERE email = '$email'");
+			}
+			catch(PDOException $e)
+			{
+                echo 'Error "<i>'.$e->getMessage().'</i></br>';
+                exit();
+            }
+
+            $_SESSION['email'] = $data['email'];
+            $_SESSION['username'] = $data['username'];
+            header('Location: '.$uri.'/index.php');
+            exit();
+		}
+		else
+		{
+			echo '<br><i> Email or password incorect !</i>';
+		}
+	}
+	else
+	{
+		echo '<br><i> Email or password incorect !</i>';
+	}
+}
 
 ?>
 </div>
