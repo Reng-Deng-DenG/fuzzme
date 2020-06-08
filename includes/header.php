d464d0b935206a56866fbd53785952070d8b3cf3
Reng-Deng-DenG <60116535+Reng-Deng-DenG@users.noreply.github.com>
2020-06-08 16:43:49 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,39 @@
-<div class='header'>
-	<h2 class='nav'> FUZZ<span class='greenMedium'>|</span>ME</h2>
-	<a href='setting.php' class='nav'>Setting</a>
-	<a href='profil.php' class='nav'>Profil</a>
-	<a href='ranking.php' class='nav'>Ranking</a>
-	<a href='index.php' class='nav'>Home</a>
-</div>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">FUZZ|ME</a>
+			</div>
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+					<li class=""><a href="index.php">Home</a></li>
+					<li class=""><a href="ranking.php">Ranking</a></li>
+					<li class=""><a href="contact.php">Contact</a></li>
+					<li class=""><a href="about.php">About</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li class="dropdown">
+						<a href="login.php" class="dropdown-toggle" data-toggle="dropdown" role="button">Sign In</a>
+						<div class="dropdown-menu" id="formLogin">
+							<div class="row">
+								<div class="container-fluid">
+								</div>
+							</div>
+						</div>
+					</li>
+					<li><a href="register.php">Register</a></li>
+				</ul>
+				
+			</div>
+		</div>
+	</nav>
